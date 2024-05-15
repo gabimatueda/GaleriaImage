174300749e227187267d2eb57c2f8a45b7ac57eb
Gabriela Mayumi Matueda <gabriela4matueda@gmail.com>
2024-05-14 21:04:32 -0300

    galeria de imagens

---
+++ src/data.ts
@@ -0,0 +1,43 @@
+export const slides = [
+    {
+      src: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dGVjaHxlbnwwfHwwfHx8MA%3D%3D',
+      title: 'Picture One',
+      description: 'Lucas Bravo',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=1120&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      title: 'Picture Two',
+      description: 'Slide Description Two',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?q=80&w=1331&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      title: 'Picture Three',
+      description: 'James Harrison',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1605379399642-870262d3d051?q=80&w=1206&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      title: 'Picture Four',
+      description: 'Fotis Fotopoulos',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?q=80&w=1228&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      title: 'Picture Five',
+      description: 'Panjak Patel',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      title: 'Picture Six',
+      description: 'Juanjo Jaramillo',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      title: 'Picture Seven',
+      description: 'Jefferson Santos',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1592424002053-21f369ad7fdb?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      title: 'Picture Eight',
+      description: 'Vipul Jha',
+    },
+    
+  ]
